f1670c66dddedfd98c475619c8dac3680aa809a7
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-08 17:02:44 -0800

    Adds store and employee validations and error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,24 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please input a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: "#{store_name}")
+
+p "Store name error: #{new_store.errors.details[:name]}"
+p "Annual revenue error: #{new_store.errors.details[:annual_revenue]}"
+
+
+puts "Please input an employee name:"
+employee_name = gets.chomp
+
+puts "Please input your hourly wage:"
+wage = gets.chomp
+
+new_employee = @store2.employees.create(first_name: "#{employee_name}", hourly_rate: wage)
+
+p "Last name error: #{new_employee.errors.details[:last_name]}"
+p "Hourly rate error: #{new_employee.errors.details[:hourly_rate]}"
+p "Store id error: #{new_employee.errors.details[:store_id]}"
