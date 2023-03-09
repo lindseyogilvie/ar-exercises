e5fe8595e89bad839099cce561488957af4657ba
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-08 16:19:07 -0800

    Finds total store revenue, average revenue and high-earning stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+total_revenue = Store.sum(:annual_revenue)
+stores_count = Store.count
+
+average_revenue = total_revenue / stores_count
+
+high_earning_stores = Store.where("annual_revenue > ?", 1000000).count
